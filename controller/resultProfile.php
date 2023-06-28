1028936d16a4bfdb356b6a992697acf74a842031
Sang Phạm <youlovelong123@gmail.com>
2023-06-28 21:11:07 +0700

    Cập nhật UserProfile
    --Update PHP MVC

---
+++ controller/resultProfile.php
@@ -0,0 +1,94 @@
+<?php
+// Kết nối tới cơ sở dữ liệu
+require_once '../config/db.php';
+$db = new connect();
+$conn = $db->getConnection();
+// Bắt đầu session
+session_start();
+
+// Xử lý dữ liệu gửi từ biểu mẫu
+if (isset($_POST['submit'])) {
+    $user_id = $_SESSION['auth'];
+    // Lấy thông tin từ biểu mẫu
+    $fullname = $_POST['fullname'];
+    $email = $_POST['email'];
+    $phone_number = $_POST['phone_number'];
+    $user_address = $_POST['user_address'];
+    $state = $_POST['state'];
+    $about = $_POST['about'];
+    $facebook = $_POST['facebook'];
+    $linkedin = $_POST['linkedin'];
+
+    // Kiểm tra xem người dùng đã chọn tệp ảnh hay chưa
+    if (isset($_FILES['avatar_url']) && $_FILES['avatar_url']['error'] === 0) {
+        $img_file = $_FILES['avatar_url'];
+        $img_name = $img_file['name'];
+        $img_size = $img_file['size'];
+        $tmp_name = $img_file['tmp_name'];
+        $error = $img_file['error'];
+
+        // Kiểm tra kích thước ảnh
+        if ($img_size > 2097152) {
+            $em = "Vui lòng chọn ảnh nhỏ hơn 2mb";
+        } else {
+            $img_ex = pathinfo($img_name, PATHINFO_EXTENSION);
+            $img_ex_lc = strtolower($img_ex);
+            $allowed_exs = array("jpg", "jpeg", "png");
+
+            // Kiểm tra phần mở rộng của tệp ảnh
+            if (in_array($img_ex_lc, $allowed_exs)) {
+                $new_img_name = uniqid("IMG-", true) . '.' . $img_ex_lc;
+                $img_upload_path = '../public/upload/users/' . $new_img_name;
+
+                // Di chuyển tệp ảnh tải lên vào thư mục đích
+                if (move_uploaded_file($tmp_name, $img_upload_path)) {
+                    // Thực hiện câu truy vấn UPDATE vào bảng "users"
+                    $query = "UPDATE users 
+                              SET fullname = :fullname, email = :email, phone_number = :phone_number, user_address = :user_address, state = :state, about = :about, facebook = :facebook, linkedin = :linkedin, avatar_url = :avatar_url
+                              WHERE user_id = :user_id";
+                    $stmt = $conn->prepare($query);
+                    $stmt->bindParam(':fullname', $fullname);
+                    $stmt->bindParam(':email', $email);
+                    $stmt->bindParam(':phone_number', $phone_number);
+                    $stmt->bindParam(':user_address', $user_address);
+                    $stmt->bindParam(':state', $state);
+                    $stmt->bindParam(':about', $about);
+                    $stmt->bindParam(':facebook', $facebook);
+                    $stmt->bindParam(':linkedin', $linkedin);
+                    $stmt->bindParam(':avatar_url', $new_img_name); // Sử dụng $new_img_name thay vì $avatar_url
+                    $stmt->bindParam(':user_id', $user_id);
+                    $stmt->execute();
+                } else {
+                    $em = "Đã xảy ra lỗi trong quá trình tải lên tệp";
+                }
+            } else {
+                $em = "Chỉ cho phép tệp JPG, JPEG và PNG";
+            }
+        }
+    } else {
+        // Người dùng không chọn tệp ảnh mới, chỉ cập nhật thông tin cá nhân
+        $query = "UPDATE users 
+                  SET fullname = :fullname, email = :email, phone_number = :phone_number, user_address = :user_address, state = :state, about = :about, facebook = :facebook, linkedin = :linkedin
+                  WHERE user_id = :user_id";
+        $stmt = $conn->prepare($query);
+        $stmt->bindParam(':fullname', $fullname);
+        $stmt->bindParam(':email', $email);
+        $stmt->bindParam(':phone_number', $phone_number);
+        $stmt->bindParam(':user_address', $user_address);
+        $stmt->bindParam(':state', $state);
+        $stmt->bindParam(':about', $about);
+        $stmt->bindParam(':facebook', $facebook);
+        $stmt->bindParam(':linkedin', $linkedin);
+        $stmt->bindParam(':user_id', $user_id);
+        $stmt->execute();
+    }
+
+    // Chuyển hướng trở lại trang profile
+    header("Location: ../profile.php");
+    exit();
+} else {
+    // Nếu không có dữ liệu gửi từ biểu mẫu, chuyển hướng trở lại trang profile
+    header("Location: ../ssss.php");
+    exit();
+}
+?>
--- view/dashboard/profile.php
+++ view/dashboard/profile.php
@@ -23,91 +23,71 @@
             
             <div class="col-lg-9 col-md-8 col-sm-12">
                 <div class="dashboard-body">
-                
+                <form action="controller/resultProfile.php" method="POST" enctype="multipart/form-data">
                     <div class="dashboard-wraper">
                     
                         <!-- Basic Information -->
                         <div class="frm_submit_block">	
-                            <h4>My Account</h4>
+                            <h4>Thông tin cá nhân</h4>
                             <div class="frm_submit_wrap">
                                 <div class="form-row">
                                 
                                     <div class="form-group col-md-6">
-                                        <label>Your Name</label>
-                                        <input type="text" class="form-control" value="Shaurya Preet">
+                                        <label>Họ & tên</label>
+                                        <input type="text" name="fullname" class="form-control" value="">
                                     </div>
                                     
                                     <div class="form-group col-md-6">
                                         <label>Email</label>
-                                        <input type="email" class="form-control" value="preet77@gmail.com">
+                                        <input type="email" name="email" class="form-control" value="">
+                                    </div>
+                                    
+                                    
+                                    <div class="form-group col-md-6">
+                                        <label>Số điện thoại</label>
+                                        <input type="text" name="phone_number" class="form-control" value="">
                                     </div>
                                     
                                     <div class="form-group col-md-6">
-                                        <label>Your Title</label>
-                                        <input type="text" class="form-control" value="Web Designer">
+                                        <label>Thành phố</label>
+                                        <input type="text" name="user_address" class="form-control" value="">
                                     </div>
                                     
                                     <div class="form-group col-md-6">
-                                        <label>Phone</label>
-                                        <input type="text" class="form-control" value="123 456 5847">
+                                        <label>Quốc tịch</label>
+                                        <input type="text" name="state" class="form-control" value="">
                                     </div>
-                                    
+
                                     <div class="form-group col-md-6">
-                                        <label>Address</label>
-                                        <input type="text" class="form-control" value="522, Arizona, Canada">
-                                    </div>
-                                    
-                                    <div class="form-group col-md-6">
-                                        <label>City</label>
-                                        <input type="text" class="form-control" value="Montquebe">
-                                    </div>
-                                    
-                                    <div class="form-group col-md-6">
-                                        <label>State</label>
-                                        <input type="text" class="form-control" value="Canada">
-                                    </div>
-                                    
-                                    <div class="form-group col-md-6">
-                                        <label>Zip</label>
-                                        <input type="text" class="form-control" value="160052">
+                                        <label>Ảnh đại diện</label>
+                                        <input type="file" name="avatar_url" required class="form-control">
                                     </div>
                                     
                                     <div class="form-group col-md-12">
-                                        <label>About</label>
-                                        <textarea class="form-control">Maecenas quis consequat libero, a feugiat eros. Nunc ut lacinia tortor morbi ultricies laoreet ullamcorper phasellus semper</textarea>
+                                        <label>Giới thiệu bản thân</label>
+                                        <textarea  name="about" class="form-control"></textarea>
                                     </div>
-                                    
                                 </div>
                             </div>
                         </div>
                         
                         <div class="frm_submit_block">	
-                            <h4>Social Accounts</h4>
+                            <h4>Liên kết mạng xã hội</h4>
                             <div class="frm_submit_wrap">
                                 <div class="form-row">
                                 
                                     <div class="form-group col-md-6">
                                         <label>Facebook</label>
-                                        <input type="text" class="form-control" value="https://facebook.com/">
-                                    </div>
-                                    
-                                    <div class="form-group col-md-6">
-                                        <label>Twitter</label>
-                                        <input type="email" class="form-control" value="https://twitter.com/">
-                                    </div>
-                                    
-                                    <div class="form-group col-md-6">
-                                        <label>Google Plus</label>
-                                        <input type="text" class="form-control" value="https://googleplus.com/">
+                                        <input type="text" name="facebook" class="form-control" value="https://facebook.com/">
                                     </div>
                                     
                                     <div class="form-group col-md-6">
                                         <label>LinkedIn</label>
-                                        <input type="text" class="form-control" value="https://linkedin.com/">
+                                        <input type="text" name="linkedin" class="form-control" value="https://linkedin.com/">
                                     </div>
                                     
                                     <div class="form-group col-lg-12 col-md-12 mt-4">
-                                        <button class="btn btn-theme btn-lg" type="submit">Save Changes</button>
+                                        <button class="btn btn-theme btn-lg" type="submit" name="submit">Cập nhật</button>
                                     </div>
                                     
                                 </div>
@@ -115,7 +95,7 @@
                         </div>
                         
                     </div>
-                
+                </form>
                 </div>
             </div>
             
